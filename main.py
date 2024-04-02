6240038ee57fe17a5c05609a79298fafbf4dea3e
James Rodgers <jrwrodgers@gmail.com>
2024-04-02 18:00:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,22 +10,46 @@ def spine(x1 ,y1 ,x2 ,y2):
     c = y1 -( m *x1)
     return m ,c
 
+def blend_bspline(self, s, params):
+    x1 = params[0]
+    y1 = params[1]
+    x2 = params[2]
+    y2 = params[3]
+    x3 = params[4]
+    y3 = params[5]
+    x4 = params[6]
+    y4 = params[7]
+    npoints = self.npoints
+    #uu = np.linspace(0, 1, num=npoints)
+    plist = [(x1, y1), (x2, y2),
+             (x3,y3), (x4, y4)]
+    ctr = np.array(plist)
+    x = ctr[:, 0]
+    y = ctr[:, 1]
+    ll = len(x)
+    t = np.linspace(0, 1, ll - 2, endpoint=True)
+    t = np.append([0, 0, 0], t)
+    t = np.append(t, [1, 1, 1])
+    tck = [t, [x, y], 3]
+    u = interpolate.splev(s, tck)
+    return u[1]
+
 
 class Wing():
-    def __init__(self,c_chord,t_chord,hspan,t_chord_y,sections):
+    def __init__(self,c_chord,t_chord,hspan,t_chord_x,sections):
         self.c_chord=c_chord
         self.t_chord=t_chord
         self.hspan=hspan
-        self.t_chord_y=t_chord_y
+        self.t_chord_x=t_chord_x
         self.nsecs=1
         self.sections=sections
-        self.m_le ,self.c_le = spine(0 ,0 ,self.hspan ,-self.t_chord_y)
-        self.m_te, self.c_te = spine(0, -self.c_chord, self.hspan, (-self.t_chord_y -self.t_chord))
+        self.m_le ,self.c_le = spine(0 ,0  ,self.t_chord_x,self.hspan) #def spine(x1 ,y1 ,x2 ,y2)
+        self.m_te, self.c_te = spine(self.c_chord,0 ,(self.t_chord_x +self.t_chord), self.hspan) #def spine(x1 ,y1 ,x2 ,y2)
         self.taper_ratio = self.t_chord / self.c_chord
-        self.area = (self.hspan * self.c_chord * (1 + self.taper_ratio)) / 1000000
-        self.le_sweep = math.atan(self.t_chord_y / self.hspan)
-        self.te_sweep = math.atan(((self.t_chord_y + self.t_chord) - self.c_chord) / self.hspan)
-        self.wing_sweep = math.atan((self.t_chord_y + (0.25 * self.t_chord) - (self.c_chord * 0.25)) / self.hspan)
+        self.area = (self.hspan * self.c_chord * (1 + self.taper_ratio)) / 1000000 #mm span to meters area
+        self.le_sweep = math.atan(self.t_chord_x / self.hspan)
+        self.te_sweep = math.atan(((self.t_chord_x + self.t_chord) - self.c_chord) / self.hspan)
+        self.wing_sweep = math.atan((self.t_chord_x + (0.25 * self.t_chord) - (self.c_chord * 0.25)) / self.hspan)
 
     def __str__(self):
         print(f"Wing Area = {self.area}")
@@ -40,19 +64,17 @@ class Wing():
             nj=20
             y_stations = [self.sections[s].ib_span_fraction*self.hspan + (j / (nj - 1)) * self.sections[s].ob_span_fraction*self.hspan for j in range(nj)]
             for j in range(len(y_stations)):
-                #print(j/(nj-1),y_stations[j])
-                c1_frac=j/(nj-1)
-                c2_frac=1-(c1_frac)
-                new_chord = np.add(self.sections[s].ib_section.points * c1_frac,self.sections[s].ob_section.points * c2_frac )
-                #print(new_chord)
-                le_offset=self.m_le*(c1_frac*self.hspan) + self.c_le
-                te_offset=self.m_te*(c1_frac*self.hspan) + self.c_te
-                scale=le_offset-te_offset
-                print(le_offset)
-                new_chord=new_chord*scale
-                new_chord[:,0]=new_chord[:,0]+le_offset
-
-                plt.plot(new_chord[:, 0],new_chord[:, 1], 'g-')
+                c2_frac=j/(nj-1)
+                c1_frac=1-(c2_frac)
+                b_chord = np.add(self.sections[s].ib_section.points * c1_frac,self.sections[s].ob_section.points * c2_frac )
+                le_offset=((c2_frac*self.hspan -self.c_le )/self.m_le)
+                te_offset=((c2_frac*self.hspan -self.c_te )/self.m_te)
+                scale=te_offset-le_offset
+                b_chord=b_chord*scale
+                b_chord[:,0]=b_chord[:,0]+le_offset
+                # rotate
+                #dihedral
+                plt.plot(b_chord[:, 0],b_chord[:, 1], 'g-')
                 #plt.plot(le_offset, y_stations[j], te_offset,y_stations[j], 'g-')
 
 
@@ -113,5 +135,4 @@ wing = Wing(400,164,1000,500,sections)
 wing.plot_wing()
 
 # Make Chord Stations
-
 
